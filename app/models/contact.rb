29b4d8e9b1cf4d0e2dbc98f8ffe569f3d3772207
andbri321 <andbri321@gmail.com>
2020-07-16 14:24:27 -0300

    [u] contact

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,15 @@
 class Contact < ApplicationRecord
+    belongs_to :kind, optional: true
+
+    # def author
+    #     'Jackson Pires'
+    # end
+
+    # def as_json(options={})
+    #     super(
+    #         root: true,
+    #         methods: [:author, :kind_description],
+    #         include: { kind: { only: :description }}
+    #     )
+    # end
 end
