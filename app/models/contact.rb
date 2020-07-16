45e47e79a5de0a15d0f6d27a4d17d4bf33d3bc49
andbri321 <andbri321@gmail.com>
2020-07-16 16:28:11 -0300

    [u] model contact

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,15 +1,14 @@
 class Contact < ApplicationRecord
     belongs_to :kind, optional: true
+    has_many :phones
 
     # def author
     #     'Jackson Pires'
     # end
 
-    # def as_json(options={})
-    #     super(
-    #         root: true,
-    #         methods: [:author, :kind_description],
-    #         include: { kind: { only: :description }}
-    #     )
-    # end
+    def as_json(options={})
+        h = super(options)
+        h[:birthdate] = (I18n.l(self.birthdate) unless self.birthdate.blank?)
+        h
+    end
 end
