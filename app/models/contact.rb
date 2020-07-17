556087df8cf6d6617fc5cf03fd42869cace8a6ed
andbri321 <andbri321@gmail.com>
2020-07-16 21:34:06 -0300

    [u] model contact

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,10 +1,13 @@
 class Contact < ApplicationRecord
     belongs_to :kind, optional: true
     has_many :phones
+    has_one :address
 
     accepts_nested_attributes_for :phones, allow_destroy: true
+    accepts_nested_attributes_for :address, update_only: true
+    
     # def author
-    #     'Jackson Pires'
+    #     'Anderson'
     # end
 
     def as_json(options={})
